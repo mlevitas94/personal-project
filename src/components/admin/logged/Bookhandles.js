cb0b2b902c845142b9e587babdfa58d672c137c9
Mark94 <mlevitas94@gmail.com>
2019-02-16 21:25:21 -0700

    input field for book control set up, no functionality

--- src/components/admin/logged/Bookhandles.js
+++ src/components/admin/logged/Bookhandles.js
@@ -11,6 +11,7 @@ class Bookhandles extends Component{
             add:{
                 title:'', purchaselink:'',imageurl:'',price:'', kprice:'', favsnip:''
             }
+           
         }
     }
 
@@ -23,7 +24,10 @@ class Bookhandles extends Component{
             }
         })
     }
+
+
     render(){
+        console.log(this.state.edit)
         const toBeDeleted = this.props.books.map((book, i) => {
             return (
                 <div key={i}>
@@ -33,55 +37,109 @@ class Bookhandles extends Component{
                 </div>
             )
         })
+
+        const toEdit = this.props.books.map((book, i) => {
+           return(
+            <div key={i}>
+                <span>{book.title}</span>
+                <br/> 
+                <button>Edit Book</button>
+
+                {/* fields pop up when clicked, hidden with overflow and height */}
+                <div className='edit-field'>
+                    <span>Title:</span>
+                        <br/>
+                        <input type='text' defaultValue={book.title}/>
+                        <br/>
+
+                        <span>Purchase Link:</span>
+                        <br/>
+                        <input type='url' defaultValue={book.link}/>
+                        <br/>
+
+                        <span>Image:</span>
+                        <br/>
+                        <input defaultValue={book.image}/>
+                        <br/>
+
+                        <span>Price:</span>
+                        <br/>
+                        <input type='text' defaultValue={book.price}/>
+                        <br/>
+
+                        <span>Kindle Price:</span>
+                        <br/>
+                        <input type='text' defaultValue={book.kindle_price}/>
+                        <br/>
+
+                        <span>Favorite Snippet:</span>
+                        <br/>
+                        <input type='text' defaultValue={book.fav_snip}/>
+                </div>
+            </div>
+           )
+
+        })
         return(
             <div className='handle-container'>
-                <button>Add a Book</button>
-                <br/>
-
-                {/* hidden with overflow and height */}
-                <div className='add-inputs'> 
-                    <span>Title:</span>
-                    <br/>
-                    <input type='text' value={this.state.add.title} onChange={(e) => this.updateAddInput( 'title',e.target.value)}/>
+                <div className='add-auth'>
+                    <button>Add a Book</button>
                     <br/>
 
-                    <span>Purchase Link:</span>
-                    <br/>
-                    <input type='url' value={this.state.add.purchaselink} onChange={(e) => this.updateAddInput( 'purchaselink',e.target.value)}/>
-                    <br/>
+                    {/* hidden with overflow and height */}
+                    <div className='add-inputs'> 
+                        <span>Title:</span>
+                        <br/>
+                        <input type='text' value={this.state.add.title} onChange={(e) => this.updateAddInput( 'title',e.target.value)}/>
+                        <br/>
 
-                    <span>Image:</span>
-                    <br/>
-                    <input value={this.state.add.imageurl} onChange={(e) => this.updateAddInput( 'imageurl',e.target.value)}/>
-                    <br/>
+                        <span>Purchase Link:</span>
+                        <br/>
+                        <input type='url' value={this.state.add.purchaselink} onChange={(e) => this.updateAddInput( 'purchaselink',e.target.value)}/>
+                        <br/>
 
-                    <span>Price:</span>
-                    <br/>
-                    <input type='text' value={this.state.add.price} onChange={(e) => this.updateAddInput( 'price',e.target.value)}/>
-                    <br/>
+                        <span>Image:</span>
+                        <br/>
+                        <input value={this.state.add.imageurl} onChange={(e) => this.updateAddInput( 'imageurl',e.target.value)}/>
+                        <br/>
 
-                    <span>Kindle Price:</span>
-                    <br/>
-                    <input type='text' value={this.state.add.kprice} onChange={(e) => this.updateAddInput( 'kprice',e.target.value)}/>
-                    <br/>
+                        <span>Price:</span>
+                        <br/>
+                        <input type='text' value={this.state.add.price} onChange={(e) => this.updateAddInput( 'price',e.target.value)}/>
+                        <br/>
 
-                    <span>Favorite Snippet:</span>
-                    <br/>
-                    <input type='text' value={this.state.add.favsnip} onChange={(e) => this.updateAddInput( 'favsnip',e.target.value)}/>
-                    <br/>
+                        <span>Kindle Price:</span>
+                        <br/>
+                        <input type='text' value={this.state.add.kprice} onChange={(e) => this.updateAddInput( 'kprice',e.target.value)}/>
+                        <br/>
+
+                        <span>Favorite Snippet:</span>
+                        <br/>
+                        <input type='text' value={this.state.add.favsnip} onChange={(e) => this.updateAddInput( 'favsnip',e.target.value)}/>
+                        <br/>
+
+                    </div>
 
                 </div>
+                <div className='delete-auth'>
+                    <button>Delete a Book</button>
+                    <br/>
 
+                    {/* hidden with overflow and height */}
+                    <div className='delete-list'>
+                        {toBeDeleted}
+                    </div>
+                </div>    
+                <div className='edit-auth'> 
+                    <button>Edit a Book</button>
+                    <br/>
+                    <div className='edit-inputs'>
+                        {toEdit}
 
-                <button>Delete a Book</button>
-                <br/>
+                    </div>
 
-                {/* hidden with overflow and height */}
-                <div className='delete-list'>
-                    {toBeDeleted}
-                </div>
-                
-                <button>Edit a Book</button>
+                </div>    
+
             </div>
         )
     }
