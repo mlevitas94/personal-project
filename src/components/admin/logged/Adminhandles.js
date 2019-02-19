090f4683c99740c7c854629a08f8ef55a62a52df
Mark94 <mlevitas94@gmail.com>
2019-02-18 20:41:30 -0700

    register inputs set up

--- src/components/admin/logged/Adminhandles.js
+++ src/components/admin/logged/Adminhandles.js
@@ -11,7 +11,66 @@ class Adminhandles extends Component{
         return(
             <div className='admin-handle-container'>
                 <h1>Register a new admin</h1>
-                
+
+                <span>First name:</span>
+
+                <br/>
+
+                <input type='text'/>
+
+                <br/>
+
+                <span>Last name:</span>
+
+                <br/>
+
+                <input type='text'/>
+
+                <br/>
+
+                <span>Username:</span>
+
+                <br/>
+
+                <input type='text'/>
+
+                <br/>
+
+                <span>Privileges:</span>
+
+                <br/>
+
+                <span>Add books:</span>
+                <input type='checkbox'/>
+
+                <br/>
+
+                <span>Delete books:</span>
+                <input type='checkbox'/>
+
+                <br/>
+
+                <span>Edit books:</span>
+                <input type='checkbox'/>
+
+                <br/>
+
+                <span>Password:</span>
+
+                <br/>
+
+                <input type='password'/>
+
+                <br/>
+
+                <span>Repeat Password:</span>
+
+                <br/>
+
+                <input type='password'/>
+
+                <br/>
+
             </div>
         )
     }
