f4bce33940c9a3e89a80b176178db3c7c637e207
Mark94 <mlevitas94@gmail.com>
2019-02-21 16:33:28 -0700

    image uploading works with add book

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
 const session = require('express-session')
+const aws = require('aws-sdk');
 const bctrl = require('./booksctrl')
 const acrtl = require('./adminctrl')
 
-const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET, S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY} = process.env
 
 const app = express();
 app.use(express.json());
@@ -31,6 +33,40 @@ app.get('/admin/getuser', acrtl.getUser)
 app.get('/admin/getadmins', acrtl.getAdmins)
 app.delete('/admin/deleteadmin/:id', acrtl.deleteAdmin)
 app.put('/admin/editadmin/:id', acrtl.editAdmin)
+
+
+//s3 image uploading
+app.get('/api/signs3', (req, res) => {
+    aws.config = {
+      region: 'us-west-1',
+      accessKeyId: AWS_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    };
+  
+    const s3 = new aws.S3();
+    const fileName = req.query['file-name'];
+    const fileType = req.query['file-type'];
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Key: fileName,
+      Expires: 60,
+      ContentType: fileType,
+      ACL: 'public-read',
+    };
+  
+    s3.getSignedUrl('putObject', s3Params, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.end();
+      }
+      const returnData = {
+        signedRequest: data,
+        url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
+      };
+  
+      return res.send(returnData);
+    });
+  });
 
 massive(CONNECTION_STRING).then(db => {
     app.set('db', db);
--- src/components/admin/logged/Bookhandles.js
+++ src/components/admin/logged/Bookhandles.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import axios from 'axios'
 import {connect} from 'react-redux'
 import {getBooks} from '../../../ducks/reducer'
+import { v4 as randomString } from 'uuid';
 import Toedit from './Toedit'
 import './Logged.css'
 
@@ -27,17 +28,72 @@ class Bookhandles extends Component{
         })
     }
 
-    addBook(){
-        const {title, purchaselink, imageurl, price, info, kprice, favsnip} = this.state.add
-        const toAdd = {title, purchaselink, imageurl, price, info, kprice, favsnip}
 
-        axios.post('/api/books', toAdd)
+    getSignedRequest = (file) => {
+        const fileName = `${randomString()}-${file.name.replace(/\s/g, '-')}`;
+
+        axios.get('api/signs3' , {
+            params: {
+                'file-name':fileName,
+                'file-type':file.type
+            }
+        })
         .then(res => {
+            const {signedRequest, url} = res.data;
+            this.uploadFile(file, signedRequest, url)
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    uploadFile = (file, signedRequest, url) => {
+        const options = {
+          headers: {
+            'Content-Type': file.type,
+          },
+        };
+    
+        axios
+          .put(signedRequest, file, options)
+          .then(res => {
+              console.log(url)
+              this.setState({
+                add: {
+                    ...this.state.add,
+                    imageurl:url
+                }
+            })
+            const {title, purchaselink, imageurl, price, info, kprice, favsnip} = this.state.add
+            const toAdd = {title, purchaselink, imageurl, price, info, kprice, favsnip}
+            axios.post('/api/books', toAdd)
+            .then(res => {
                 this.setState({
                     add: { title: '',purchaselink: '',imageurl:'',price: '',info:'', kprice: '',favsnip:''}
                 }) 
                 this.props.getBooks(res.data)  
-        })
+            }).catch(err => {
+                console.log(err)
+            })  
+          })
+          .catch(err => {
+            if (err.response.status === 403) {
+              alert(
+                `Your request for a signed URL failed with a status 403. Double check the CORS configuration and bucket policy in the README. You also will want to double check your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in your .env and ensure that they are the same as the ones that you created in the IAM dashboard. You may need to generate new keys\n${
+                  err.stack
+                }`
+              );
+            } else {
+              alert(`ERROR: ${err.status}\n ${err.stack}`);
+            }
+          });
+      };
+    
+
+    async addBook(){
+        const imageInput = document.getElementById('imageInput').files[0]
+        try{await this.getSignedRequest(imageInput)}catch(err){console.log(err)}
     }
     deleteBook(id){
         axios.delete(`/api/books/${id}`)
@@ -58,6 +114,8 @@ class Bookhandles extends Component{
             console.log(err)
         })
     }
+
+    
 
 
     render(){
@@ -108,7 +166,7 @@ class Bookhandles extends Component{
 
                         <span>Image:</span>
                         <br/>
-                        <input value={this.state.add.imageurl} onChange={(e) => this.updateAddInput( 'imageurl',e.target.value)}/>
+                        <input id='imageInput' type='file' accept='image/*'/>
                         <br/>
 
                         <span>Price:</span>
