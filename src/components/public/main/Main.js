dc28a1f0df196fe72cf16bd80ef7a0531989e195
Mark94 <mlevitas94@gmail.com>
2019-02-24 17:46:54 -0700

    home page up to par

--- src/components/public/main/Main.js
+++ src/components/public/main/Main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Main.css'
+import './Main.scss'
 import routes from './routes'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
--- src/components/public/main/routed-container/home/Home.js
+++ src/components/public/main/routed-container/home/Home.js
@@ -1,33 +1,44 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import './Home.scss'
 
 
 const Home = (props) =>{
+    console.log(props)
     const bookList = props.books.map((book,i) =>{
         return(
-            <div className='book'
-                key={i}>
+            <div className='book' key={i}>
+                <h3>{book.title}</h3>
+                <div className='halfed'>
+                    <div className='left'>
+                        <br/>
+                        <img src={`${book.image}`}/>
+                        <br/>
+                    </div>
+                    <div className='right'>
+                        <span>Price: ${book.price}.00</span>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <span>Kindle Price: ${book.kindle_price}.00</span>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <a target='_blank' href={book.link}> Purchase Book</a>
+                    </div>
+                </div>
                 <br/>
-                <Link to={`/books/${book.book_id}`}>Link to inidividual book</Link>
+                <Link to={`/books/${book.book_id}`}>More Information</Link>
                 <br/>
-                {book.title}
                 <br/>
-                {book.price}
-                <br/>
-                
-
             </div>
         )
     })
     return(
-        <div>
-            this is the home page
-            <br/>
             <div className='books-container'>
                 {bookList}
             </div>
-        </div>
     )
 }
 const mapToProps = reduxState =>{
